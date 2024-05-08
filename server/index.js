5b68fe0b5246d81589d9c6c9f6611d0f14fa7306
Hamad Adel <hamadwebdeveloper@gmail.com>
2024-05-08 20:49:54 +0300

    store application environment variables into .env file and connect to mongodb using mongoose package

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-const port = process.env.PORT || 3000;
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
 const apiRoutes = require('./routes');
 const app = express();
 
+mongoose
+  .connect(process.env.CONNECTION_STRING, {})
+  .then(() => console.log('DB connected'))
+  .catch((err) => console.log('DB Error => ', err));
+
+// Express middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+// app.use(cors()); // allow origins to make requests to this server
+if (process.env.NODE_ENV == 'development') {
+  app.use(cors({ origin: 'http://localhost:3001' }));
+}
+
 app.use('/api', apiRoutes);
 
-app.listen(port, () => {
-  console.log(`Server started at http://127.0.0.1:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server started at http://127.0.0.1:${PORT}`);
 });
